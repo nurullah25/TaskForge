764997545cc41c42407864c0171a7d3c99f136b4
nurullah25 <nurullah25@users.noreply.github.com>
2026-09-22 22:25:16 +0600

    Add solution and Web API project

---
+++ server/TaskForge.Api/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHealthChecks("/api/health");
+
+app.Run();
+
+// Makes Program visible to WebApplicationFactory in the test project.
+public partial class Program { }
